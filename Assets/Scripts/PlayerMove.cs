3b9fd35fea680b496eb573f7b25d13ec8c00d9d4
galatsu <tracy4fun.minecraft@gmail.com>
2022-10-02 21:27:45 -0400

    checking for script mistakes

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
-{
+{ 
     Rigidbody2D myBody;
     float horizontalMove;
 
     public float moveMult;
 
     bool grounded = false;
-    public float castDist = 0.5f;
+    public float castDist = 0.2f;
 
     public float jumpLimit = 2f;
     public float gravityScale = 5f;
@@ -20,68 +20,68 @@ public class PlayerMove : MonoBehaviour
 
     Animator myAnim;
 
-    // Start is called before the first frame update
-    void Start()
+// Start is called before the first frame update
+void Start()
+{
+    myBody = GetComponent<Rigidbody2D>();
+    myAnim = GetComponent<Animator>();
+}
+
+// Update is called once per frame
+void Update()
+{
+    horizontalMove = Input.GetAxis("Horizontal");
+    Debug.Log(GetComponent<SpriteRenderer>().size);
+    if (Input.GetButtonDown("Jump") && grounded)
     {
-        myBody = GetComponent<Rigidbody2D>();
-        myAnim = GetComponent<Animator>();
+        jump = true;
+    }
+}
+
+private void FixedUpdate()
+{
+    HorizontalMove(horizontalMove);
+
+    if (jump)
+    {
+        myBody.AddForce(Vector2.up * jumpLimit, ForceMode2D.Impulse);
+        jump = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    if (myBody.velocity.y >= 0)
     {
-        horizontalMove = Input.GetAxis("Horizontal");
-        Debug.Log(GetComponent<SpriteRenderer>().size);
-        if (Input.GetButtonDown("Jump") && grounded)
-        {
-            jump = true;
-        }
+        myBody.gravityScale = gravityScale;
+    }
+    else if (myBody.velocity.y < 0)
+    {
+        myBody.gravityScale = gravityFall;
     }
 
-    private void FixedUpdate()
+    RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, castDist);
+    Debug.DrawRay(transform.position, Vector2.down * castDist, new Color(255, 0, 0));
+    if (hit.collider != null && hit.transform.name == "Terrain")
     {
-        HorizontalMove(horizontalMove);
-
-        if (jump)
-        {
-            myBody.AddForce(Vector2.up * jumpLimit, ForceMode2D.Impulse);
-            jump = false;
-        }
-
-        if (myBody.velocity.y >= 0)
-        {
-            myBody.gravityScale = gravityScale;
-        }
-        else if (myBody.velocity.y < 0)
-        {
-            myBody.gravityScale = gravityFall;
-        }
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, castDist);
-        Debug.DrawRay(transform.position, Vector2.down * castDist, new Color(255, 0, 0));
-        if (hit.collider != null && hit.transform.name == "Terrain")
-        {
-            grounded = true;
-            //Debug.Log(hit.transform.name);
-        }
-        else
-        {
-            grounded = false;
-        }
-
+        grounded = true;
+        //Debug.Log(hit.transform.name);
+    }
+    else
+    {
+        grounded = false;
     }
 
-    void HorizontalMove(float toMove)
+}
+
+void HorizontalMove(float toMove)
+{
+    float moveX = toMove * Time.fixedDeltaTime * moveMult;
+    myBody.velocity = new Vector3(moveX, myBody.velocity.y);
+    if (myBody.velocity.x > 0 || myBody.velocity.x < 0)
     {
-        float moveX = toMove * Time.fixedDeltaTime * moveMult;
-        myBody.velocity = new Vector3(moveX, myBody.velocity.y);
-        if (myBody.velocity.x > 0 || myBody.velocity.x < 0)
-        {
-            myAnim.SetBool("isWalking", true);
-        }
-        else
-        {
-            myAnim.SetBool("isWalking", false);
-        }
+        myAnim.SetBool("isWalking", true);
     }
+    else
+    {
+        myAnim.SetBool("isWalking", false);
+    }
+}
 }
