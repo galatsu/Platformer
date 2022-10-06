9ae3f24290837e535c297be6993b5698fc309f9c
galatsu <tracy4fun.minecraft@gmail.com>
2022-10-05 23:26:10 -0400

    finishing touches

--- Assets/Scripts/TokenCont.cs
+++ Assets/Scripts/TokenCont.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TokenCont : MonoBehaviour
 {
@@ -17,5 +18,6 @@ public class TokenCont : MonoBehaviour
             scoreManager.score += 1f;
             Destroy(gameObject);
         }
+
     }
 }
